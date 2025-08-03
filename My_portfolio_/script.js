634113e11e3614b03486cc7f5ac91acaaacc953e
Manjeet varun <147270450+Manjeet94565@users.noreply.github.com>
2025-08-03 19:01:05 +0530

    Add files via upload
    create a amazing personal portfolio// using html, css ,js languages

---
+++ My_portfolio_/script.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav ul li ');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav ul li [href*=' + id + ']').classList.add
+                ('active');
+            });
+        };
+    });
+}; 
